324611e1cf8f354d089745b38dffddf61b49695e
nikchernyakov <nikchernyakov@yandex.ru>
2019-07-04 00:37:34 +0300

    Fix error with namespace

--- Editor/Inspector/SeparableImageEditor.cs
+++ Editor/Inspector/SeparableImageEditor.cs
@@ -1,28 +1,25 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-namespace Editor.Inspector
+[CustomEditor(typeof(SeparableImage))]
+public class SeparableImageEditor : UnityEditor.Editor
 {
-    [CustomEditor(typeof(SeparableImage))]
-    public class SeparableImageEditor : UnityEditor.Editor
+    public override void OnInspectorGUI()
     {
-        public override void OnInspectorGUI()
+        DrawDefaultInspector();
+    
+        var script = (SeparableImage) target;
+        if(GUILayout.Button("Init"))
         {
-            DrawDefaultInspector();
-        
-            var script = (SeparableImage) target;
-            if(GUILayout.Button("Init"))
-            {
-                script.Init();
-            }
-            if(GUILayout.Button("Next"))
-            {
-                script.Increase(1);
-            }
-            if(GUILayout.Button("Previous"))
-            {
-                script.Decrease(1);
-            }
+            script.Init();
+        }
+        if(GUILayout.Button("Next"))
+        {
+            script.Increase(1);
+        }
+        if(GUILayout.Button("Previous"))
+        {
+            script.Decrease(1);
         }
     }
 }
